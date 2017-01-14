44f197ca024f8c9663f1802d27e9c1f2b3d2ae63
motygullin_503 <bul3515@gmail.com>
2017-01-14 14:24:01 +0300

    initial commit(2)

---
+++ app/src/main/java/ru/itis/marvel_encyclopedia/activities/MainActivity.java
@@ -0,0 +1,10 @@
+package ru.itis.marvel_encyclopedia.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by UseR7 on 14.01.2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
